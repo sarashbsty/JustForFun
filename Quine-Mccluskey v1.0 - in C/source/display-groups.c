b309053be3aa13d53ba36c2cfdff75c69596d28d
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2025-09-09 20:06:43 +0530

    Update display-groups.c
    more cosmetic changes

--- Quine-Mccluskey v1.0 - in C/source/display-groups.c
+++ Quine-Mccluskey v1.0 - in C/source/display-groups.c
@@ -6,29 +6,21 @@
 void displayGroups(quine group[] , int variables){
 	
 	int line = 2+5+3+20+3+15+3+8+2;
-    char str_line[1000];
+    char str_line[500] , str_line2[500];
 	if (line >= sizeof(str_line)) line = sizeof(str_line) - 1; // prevent overflow
 	
 	memset(str_line, '=', line);
     str_line[line] = '\0';
+	memset(str_line2, '-', line);
+    str_line2[line] = '\0';
 	
-	// separator line at start
 	printf("%s\n",str_line);
+	printf("| %-5s | %-20s | %-15s | %-8s |\n", "Group", "MinTerm", "Binary", "Combined");
+    printf("%s\n",str_line);
 	
-	printf("| %-5s | %-20s | %-15s | %-8s |", "Group", "MinTerm", "Binary", "Combined");
-
-//	printf("%-2s", "|");
-//  printf("%-5s%-3s", "Group", " |");
-//  printf("%-20s%-3s", "MinTerm", " |");
- // printf("%-15s%-3s", "Binary", " |");
-//	printf("%-8s%2s", "Combined" , "|");
-	printf("\n");
-         
     for (int i = 0; i <= variables; i++) {
-        if (group[i].count == 0) continue; // skip empty groups
 		
-		// separator line before every group
-		printf("%s\n",str_line);  
+        if (group[i].count == 0) continue; // skip empty groups
 		
         for (int j = 0; j < group[i].count; j++) {
 		
@@ -40,26 +32,17 @@ void displayGroups(quine group[] , int variables){
 				int written = snprintf(str+offset , sizeof(str)-offset , (k < size-1) ? "%d," : "%d" , num);
 				offset += written;
             }
-			
-			printf("%-2s", "|");
-            if (j == 0) 
-                printf("%-5d",i) ; // print group number only once
-            else 
-                printf("%-5s"," ");
-			printf("%-3s"," |");
-			
-			
-            printf("%-20s%-3s", str , " |");
-			printf("%-15s%-3s", group[i].binary[j] , " |");
-			
-			if(group[i].combined[j] == 0) printf("%-9s","❌");
-			else printf("%-9s","✅");                           
-			
-			printf("%2s\n" , "|" );
+					
+			char *symbol = (group[i].combined[j] == 0) ? "❌" : "✅";  // in conditional the symbols decay to char* type
+			if(j == 0)
+				printf("| %-5d | %-20s | %-15s | %-9s |", i , str , group[i].binary[j] , symbol);
+			else
+				printf("| %-5s | %-20s | %-15s | %-9s |", " " , str , group[i].binary[j] , symbol);
+			printf("\n");
                  
-        }        
+        }
+		
+		// separator line after every group
+		printf("%s\n",str_line2);  
     }
-	
-	// separator line at end
-    printf("%s\n",str_line);;
 }
